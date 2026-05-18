0cefd971de962974890fbfb033f395ebf9cc538c
HTTPauloGoncalves <paulomontealtomac@gmail.com>
2026-05-18 20:24:26 -0300

    feat/yaml: funções para manipular arquivo de configuração, YAML

--- internal/config/yaml.go
+++ internal/config/yaml.go
@@ -45,7 +45,7 @@ func SaveConfig(path string, cfg Config) error {
 	return os.WriteFile(path, data, 0644)
 }
 
-func LoadConfig(path string) (Config, error) {
+func loadConfig(path string) (Config, error) {
 	if path == "" {
 		path = defaultPath
 	}
@@ -76,10 +76,10 @@ func LoadConfig(path string) (Config, error) {
 }
 
 func AddServer(path string, name string, server Server) error {
-	cfg, err := LoadConfig(path)
+	cfg, err := loadConfig(path)
 
 	if err != nil {
-		return fmt.Errorf("Erro ao carregar as configurações")
+		return fmt.Errorf("Erro ao carregar as configurações. %s", err)
 	}
 
 	if name == "" {
@@ -96,10 +96,10 @@ func AddServer(path string, name string, server Server) error {
 }
 
 func AddService(path string, name string, service Service) error {
-	cfg, err := LoadConfig(path)
+	cfg, err := loadConfig(path)
 
 	if err != nil {
-		return fmt.Errorf("Erro ao carregar as configurações")
+		return fmt.Errorf("Erro ao carregar as configurações. %s", err)
 	}
 
 	if name == "" {
@@ -113,6 +113,122 @@ func AddService(path string, name string, service Service) error {
 	cfg.Services[name] = service
 
 	return SaveConfig(path, cfg)
+}
+
+func GetServer(path string, name string) (Server, error) {
+	cfg, err := loadConfig(path)
+
+	if err != nil {
+		return Server{}, err
+	}
+
+	server, exist := cfg.Servers[name]
+
+	if !exist {
+		return Server{}, fmt.Errorf("O servidor %s não foi encontrado", name)
+	}
+
+	return server, nil
+}
+
+func GetService(path string, name string) (Service, error) {
+	cfg, err := loadConfig(path)
+
+	if err != nil {
+		return Service{}, fmt.Errorf("Erro ao carregar as configurações. %s", err)
+	}
+
+	service, exist := cfg.Services[name]
+
+	if !exist {
+		return Service{}, fmt.Errorf("O deploy %s não foi encontrado", err)
+	}
+
+	return service, nil
+}
+
+func ListServers(path string) (map[string]Server, error) {
+	cfg, err := loadConfig(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg.Servers, nil
+}
+
+func ListServices(path string) (map[string]Service, error) {
+	cfg, err := loadConfig(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg.Services, nil
+}
+
+func RemoveService(path string, name string) error {
+	cfg, err := loadConfig(path)
+
+	if err != nil {
+		return err
+	}
+
+	delete(cfg.Services, name)
+
+	SaveConfig(path, cfg)
+
+	return nil
+}
+
+func RemoveServer(path string, name string) error {
+	cfg, err := loadConfig(path)
+
+	if err != nil {
+		return err
+	}
+
+	delete(cfg.Servers, name)
+
+	SaveConfig(path, cfg)
+
+	return nil
+}
+
+func UpdateService(path string, name string, service Service) error {
+	cfg, err := loadConfig(path)
+
+	if err != nil {
+		return err
+	}
+
+	if _, exist := cfg.Services[name]; !exist {
+		return fmt.Errorf("O serviço de deploy %s não existe", name)
+	}
+
+	cfg.Services[name] = service
+
+	SaveConfig(path, cfg)
+
+	return nil
+}
+
+func UpdateServer(path string, name string, service Server) error {
+	cfg, err := loadConfig(path)
+
+	if err != nil {
+		return err
+	}
+
+	if _, exist := cfg.Servers[name]; !exist {
+		return fmt.Errorf("O servidor %s não existe", name)
+	}
+
+	cfg.Servers[name] = service
+
+	SaveConfig(path, cfg)
+
+	return nil
 }
 
 func FileExists(path string) bool {
