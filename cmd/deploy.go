577f47f94b95383e14bd4155bd5628a271cd140b
HTTPauloGoncalves <paulomontealtomac@gmail.com>
2026-05-23 10:13:33 -0300

    feat/adicionando confirmação no deploy

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -18,6 +18,7 @@ var deployCmd = &cobra.Command{
 	Args:  cobra.MaximumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		serviceName := ""
+		var confirm bool
 
 		if len(args) > 0 {
 			serviceName = args[0]
@@ -54,6 +55,19 @@ var deployCmd = &cobra.Command{
 		err := internal.Deploy(serviceName)
 		if err != nil {
 			fmt.Println("Erro:", err)
+			return
+		}
+		survey.AskOne(&survey.Confirm{
+			Message: "O deploy foi executado com sucesso. Deseja marcar o deploy como finalizado?",
+			Default: true,
+		}, &confirm)
+		if err != nil {
+			fmt.Println("Erro:", err)
+			return
+		}
+
+		if !confirm {
+			fmt.Println("O deploy não foi marcado como finalizado.")
 			return
 		}
 
